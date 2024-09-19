f004cb516837a0e07c01d7bafa7e66f093f35738
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-09-19 22:42:17 +0700

    2.0.0: added brawler's special for katana

--- core/src/main/java/com/zrp200/rkpd2/items/weapon/melee/Katana.java
+++ core/src/main/java/com/zrp200/rkpd2/items/weapon/melee/Katana.java
@@ -21,12 +21,21 @@
 
 package com.zrp200.rkpd2.items.weapon.melee;
 
+import com.watabou.utils.PathFinder;
+import com.watabou.utils.Random;
 import com.zrp200.rkpd2.Assets;
+import com.zrp200.rkpd2.Dungeon;
+import com.zrp200.rkpd2.actors.Actor;
 import com.zrp200.rkpd2.actors.Char;
 import com.zrp200.rkpd2.actors.hero.Hero;
 import com.zrp200.rkpd2.actors.hero.Talent;
+import com.zrp200.rkpd2.actors.hero.abilities.rogue.ShadowClone;
+import com.zrp200.rkpd2.actors.mobs.npcs.MirrorImage;
 import com.zrp200.rkpd2.messages.Messages;
+import com.zrp200.rkpd2.scenes.GameScene;
 import com.zrp200.rkpd2.sprites.ItemSpriteSheet;
+
+import java.util.ArrayList;
 
 public class Katana extends MeleeWeapon implements Talent.SpellbladeForgeryWeapon {
 
@@ -50,6 +59,34 @@ public class Katana extends MeleeWeapon implements Talent.SpellbladeForgeryWeapo
 	}
 
 	@Override
+	public int warriorAttack(int damage, Char enemy) {
+
+		int attack = super.warriorAttack(damage, enemy);
+
+		ArrayList<Integer> respawnPoints = new ArrayList<>();
+
+		for (int i = 0; i < PathFinder.NEIGHBOURS8.length; i++) {
+			int p = enemy.pos + PathFinder.NEIGHBOURS8[i];
+			if (Actor.findChar( p ) == null && Dungeon.level.passable[p]) {
+				respawnPoints.add( p );
+			}
+		}
+
+		if (respawnPoints.size() > 0) {
+			int index = Random.index( respawnPoints );
+
+			AfterImage mob = new AfterImage();
+			mob.duplicate( Dungeon.hero );
+			GameScene.add( mob );
+			ShadowClone.ShadowAlly.appear( mob, respawnPoints.get( index ) );
+
+			respawnPoints.remove( index );
+		}
+
+		return attack;
+	}
+
+	@Override
 	public String targetingPrompt() {
 		return Messages.get(this, "prompt");
 	}
@@ -57,5 +94,30 @@ public class Katana extends MeleeWeapon implements Talent.SpellbladeForgeryWeapo
 	@Override
 	protected void duelistAbility(Hero hero, Integer target) {
 		Rapier.lungeAbility(hero, target, 1.35f, 0, this);
+	}
+
+	private static class AfterImage extends MirrorImage {
+
+		{
+			actPriority = HERO_PRIO-1;
+		}
+
+		@Override
+		public int attackProc(Char enemy, int damage) {
+			int dmg = super.attackProc(enemy, damage)*2;
+			destroy();
+			sprite.die();
+			return dmg;
+		}
+
+		@Override
+		public boolean isInvulnerable(Class effect) {
+			return true;
+		}
+
+		@Override
+		public float speed() {
+			return super.speed()*2f;
+		}
 	}
 }
