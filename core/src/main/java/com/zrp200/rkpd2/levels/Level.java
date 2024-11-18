263349aedb0c83085bacc12a706de27c400acae3
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-11-18 14:35:55 +0700

    2.0.2: trying to fix forbidden runes

--- core/src/main/java/com/zrp200/rkpd2/levels/Level.java
+++ core/src/main/java/com/zrp200/rkpd2/levels/Level.java
@@ -235,7 +235,7 @@ public abstract class Level implements Bundlable {
 				//TODO while this does significantly reduce this challenge's levelgen impact, it doesn't quite remove it
 				//for 0 levelgen impact, we need to do something like give the player all SOU, but nerf them
 				//or give a random scroll (from a separate RNG) instead of every 2nd SOU
-				if (Dungeon.branch != AbyssLevel.BRANCH || !Dungeon.isChallenged(Challenges.NO_SCROLLS) || Dungeon.LimitedDrops.UPGRADE_SCROLLS.count%2 != 0){
+				if (Dungeon.branch != AbyssLevel.BRANCH || (!Dungeon.isChallenged(Challenges.NO_SCROLLS) || Dungeon.LimitedDrops.UPGRADE_SCROLLS.count%2 != 0)){
 					addItemToSpawn(new ScrollOfUpgrade());
 				}
 			}
