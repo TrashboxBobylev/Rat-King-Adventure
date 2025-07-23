75a3cf50b55f052355aef9ea62c4abd34f1e7d59
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-07-24 01:58:08 +0700

    2.2.3: attempting to fix the issue with out-of-bounds fire

--- core/src/main/java/com/zrp200/rkpd2/actors/blobs/Fire.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/blobs/Fire.java
@@ -51,54 +51,56 @@ public class Fire extends Blob {
 
 		for (int i = area.left-1; i <= area.right; i++) {
 			for (int j = area.top-1; j <= area.bottom; j++) {
-				cell = i + j*Dungeon.level.width();
-				if (Dungeon.level.insideMap(cell) && cur[cell] > 0) {
+				cell = i + j * Dungeon.level.width();
+				if (Dungeon.level.insideMap(cell)) {
+					if (cur[cell] > 0) {
 
-					if (freeze != null && freeze.volume > 0 && freeze.cur[cell] > 0){
-						freeze.clear(cell);
-						off[cell] = cur[cell] = 0;
-						continue;
-					}
+						if (freeze != null && freeze.volume > 0 && freeze.cur[cell] > 0) {
+							freeze.clear(cell);
+							off[cell] = cur[cell] = 0;
+							continue;
+						}
 
-					burn( cell, fireClass );
+						burn(cell, fireClass);
 
-					fire = cur[cell] - 1;
+						fire = cur[cell] - 1;
 
-					if (Dungeon.isChallenged(Challenges.BURN)){
-						fire = cur[cell];
-					}
+						if (Dungeon.isChallenged(Challenges.BURN)) {
+							fire = cur[cell];
+						}
 
-					if (fire <= 0 && flamable[cell]) {
+						if (fire <= 0 && flamable[cell]) {
 
-						Dungeon.level.destroy( cell );
+							Dungeon.level.destroy(cell);
 
-						observe = true;
-						GameScene.updateMap( cell );
+							observe = true;
+							GameScene.updateMap(cell);
 
-					}
+						}
 
-			} else if ((freeze == null || freeze.volume <= 0 || freeze.cur[cell] <= 0) && !Dungeon.isChallenged(Challenges.BURN)) {
+					} else if ((freeze == null || freeze.volume <= 0 || freeze.cur[cell] <= 0) && !Dungeon.isChallenged(Challenges.BURN)) {
 
-					if (flamable[cell]
-							&& (cur[cell-1] > 0
-							|| cur[cell+1] > 0
-							|| cur[cell-Dungeon.level.width()] > 0
-							|| cur[cell+Dungeon.level.width()] > 0)) {
-						fire = 4;
-						burn( cell, fireClass );
-						area.union(i, j);
+						if (flamable[cell]
+								&& (cur[cell - 1] > 0
+								|| cur[cell + 1] > 0
+								|| cur[cell - Dungeon.level.width()] > 0
+								|| cur[cell + Dungeon.level.width()] > 0)) {
+							fire = 4;
+							burn(cell, fireClass);
+							area.union(i, j);
+						} else {
+							fire = 0;
+						}
+
 					} else {
-						fire = 0;
+						if (!Dungeon.isChallenged(Challenges.BURN))
+							fire = 0;
 					}
-
-				} else {
 					if (!Dungeon.isChallenged(Challenges.BURN))
-						fire = 0;
+						volume += (off[cell] = fire);
+					else
+						volume += 1;
 				}
-				if (!Dungeon.isChallenged(Challenges.BURN))
-					volume += (off[cell] = fire);
-				else
-					volume += 1;
 			}
 		}
 
